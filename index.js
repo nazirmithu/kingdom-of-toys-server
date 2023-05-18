05c499e2ae593c97d7ab4a422769d4d664b8d794
nazirmithu <nazirmithu22@gmail.com>
2023-05-19 01:56:14 +0600

    kingdom of toys server seide setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+    res.send('kingdom of joys server is running')
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`kingdom server is running on port:${port}`)
+})
