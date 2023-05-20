c237ff3555cb94389c1a653004e537aada803efe
nazirmithu <nazirmithu22@gmail.com>
2023-05-20 19:47:36 +0600

    updated option add

--- index.js
+++ index.js
@@ -67,16 +67,32 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/alltoys/:id',async(req, res)=>{
+    app.get('/alltoys/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await toysCollection.findOne(query);
       res.send(result);
     })
 
-    app.delete('/mytoys/:id', async(req,res)=>{
-      const id=req.params.id;
-      const query ={_id: new ObjectId(id)}
+    app.put('/alltoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const newUpdatedToy= req.body;
+      const updatedToy = {
+        $set: {
+          price: newUpdatedToy.price,
+          quantity: newUpdatedToy.quantity,
+          description: newUpdatedToy.description
+        }
+      }
+      const result= await toysCollection.updateOne(filter,options,updatedToy)
+      res.send(result)
+    })
+
+    app.delete('/mytoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
       const result = await toysCollection.deleteOne(query);
       res.send(result);
     })
